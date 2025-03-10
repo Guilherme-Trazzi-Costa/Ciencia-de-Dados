10dc8b4840ffa26b5b564b3db7ae67e342550770
GuiTCosta <guitcosta342@gmail.com>
2025-03-10 20:03:10 -0300

    Delete For_Ex2.py

--- For_Ex2.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que receba dois números inteiros e gere os números
-#inteiros que estão no intervalo compreendido por eles.
-
-n1 = int(input('Insira um numero: '))
-n2 = int(input('Insira um numero: '))
-
-for j in range(n1+1, n2):
-    print(j)
