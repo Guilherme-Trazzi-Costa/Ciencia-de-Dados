c8599d101f31705bd3d53bf6381af73751122625
Guilherme Trazzi Costa <guitcosta342@gmail.com>
2025-09-12 21:54:25 -0300

    Add files via upload

--- API/API Clinica VET/vet.py
+++ API/API Clinica VET/vet.py
@@ -2,6 +2,9 @@ from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+
+
+#Tabela Clientes
 app = Flask("clientes")
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
@@ -116,12 +119,138 @@ def atualiza_carro(id_cliente_pam):
         return gera_resposta(400, {}, "Erro ao atualizar!")
 
 
+#--------------------------------------------------------------------------------------------
+
+#Chave Estrangeira
+#id_cliente = mybd.Column(mybd.Integer, mybd.ForeignKey("tb_clientes.id_cliente"), nullable = False)
+
+#Tabela Pets
+
+
+class Pets(mybd.Model):
+    __tablename__ = "tb_pets"
+    id_pet = mybd.Column(mybd.Integer, primary_key = True)
+    nome = mybd.Column(mybd.String(255))
+    tipo = mybd.Column(mybd.String(255))
+    raca = mybd.Column(mybd.String(255))
+    data_nascimento = mybd.Column(mybd.String(255))
+    id_clienteP = mybd.Column(mybd.Integer, mybd.ForeignKey("tb_clientes.id_clienteP"), nullable = False)
+
+    def to_json_pets(self):
+        return{
+            "id_pet": self.id_pet,
+            "nome": self.nome,
+            "tipo":self.tipo,
+            "raca": self.raca,
+            "data_nascimento": str(self.data_nascimento),
+            "id_clienteP": self.id_clienteP
+
+        }
+
+#--------------------------------------------------------------
+#GET
+
+@app.route("/pets", methods = ["GET"])
+
+def seleciona_pet():
+    pet_selecionado = Pets.query.all()
+    pet_json = [pet.to_json_pets()
+                for pet in pet_selecionado]
+    return gera_resposta(200, "pets", pet_json)
+
+
+#---------------------------------------------------------------
+#GET por id
+
+@app.route("/pets/<id_pet_pam>", methods = ["GET"])
+def seleciona_pet_id(id_pet_pam):
+    pet_selecionado = Pets.query.filter_by(id_pet = id_pet_pam).first()
+    pet_json = pet_selecionado.to_json_pets()
+
+    return gera_resposta(200, pet_json, "Pet Encontrado!")
+
+
+#--------------------------------------------------------------
+# POST
+
+@app.route("/pets", methods = ["POST"])
+def criar_pet():
+    requisicao = request.get_json()
+
+    try:
+        pet = Pets(
+            id_pet =requisicao["id_pet"],
+            nome = requisicao["nome"],
+            tipo = requisicao["tipo"],
+            raca = requisicao["raca"],
+            data_nascimento = requisicao["data_nascimento"],
+            id_clienteP = requisicao["id_clienteP"]
+        )
+
+        mybd.session.add(pet)
+
+        mybd.session.commit()
+
+        return gera_resposta(201, pet.to_json_pets(), "Criado com Sucesso!")
+    
+    except Exception as e:
+        print("Erro", e)
+
+        return gera_resposta(400, {}, "Erro ao cadastrar!")
+    
+    #-----------------------------------------------------------
+    #DELETE
+
+@app.route("/pets/<id_pet_pam>", methods = ["DELETE"])
+def deleta_pet(id_pet_pam):
+    pet = Pets.query.filter_by(id_pet = id_pet_pam).first()
+
+    try:
+        mybd.session.delete(pet)
+
+        mybd.session()
+
+        return gera_resposta(200, pet.to_json_pets(), "Deletado com Sucesso")
+
+    except Exception as e:
+        print("Erro", e)
+        return gera_resposta(400, {}, "Erro ao Deletar!")
+    
+#-------------------------------------------------------------------
+#PUT
+@app.route("/pets/<id_pet_pam>", methods= ["PUT"])
+def atualiza_pet(id_pet_pam):
+    pet = Pets.query.filter_by(id_pet = id_pet_pam).first()
+    requisicao = request.get_json()
+
+    try:
+        if("nome" in requisicao):
+            pet.nome = requisicao["nome"]
+        if("tipo" in requisicao):
+            pet.tipo = requisicao["tipo"]
+        if("raca" in requisicao):
+            pet.raca = requisicao["raca"]
+        if("data_nascimento" in requisicao):
+            pet.data_nascimento = requisicao["data_nascimento"]
+
+        mybd.session.add(pet)
+
+        mybd.session.commit()
+
+        return gera_resposta(200, pet.to_json_pet(), "Pet atualizado !")
+    
+    except Exception as e:
+        print("Erro", e)
+        return gera_resposta(400, {}, "Erro ao atualizar!")
+    
+
+
 
 
 #RESPOSTA
 def gera_resposta(status, conteudo, mensagem=False):
     body = {}
-    body["Lista Clientes"] = conteudo
+    body["Lista"] = conteudo
     if(mensagem):
         body["mensagem"] = mensagem
 
